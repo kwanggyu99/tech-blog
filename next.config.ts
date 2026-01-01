86d0130e726087a56eb38e2ac5fb061adfe6a158
kwanggyu99 <91644564+kwanggyu99@users.noreply.github.com>
2026-01-01 18:06:34 +0900

    enable static export for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",               // ✅ out/ 생성
+  images: { unoptimized: true },  // ✅ GitHub Pages에서 next/image 문제 방지
+  basePath: "/tech-blog",         // ✅ repo 이름이 tech-blog라서
+  assetPrefix: "/tech-blog/",
+  trailingSlash: true,
 };
 
 export default nextConfig;
